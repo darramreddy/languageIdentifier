b2d5f2464c62d63763659dfb79c8b6ff133076a5
Ekdatha Arramreddy <darram@umich.edu>
2024-01-31 18:44:56 -0500

    Added token frequency dict

--- Assignment1/darram.Assignment1/preprocess.py
+++ Assignment1/darram.Assignment1/preprocess.py
@@ -183,94 +183,72 @@ def tokenizeText(input_string):
         final_tokens.append(current_token)
     return final_tokens
 
-def calculate_pair_freqs(tokens, vocab, all_tokens):
-    pairs = Counter()
+def calculate_pair_freqs(tokens_dict, vocab, all_tokens):
+    pairs = {}
     most_freq_word = ""
     highest_freq = -1
+    first_word = ""
+    second_word = ""
 
-    # Create a set for faster membership checks
-    vocab_set = set(vocab.keys())
+    vocab_set = set(vocab)
+    pairs_checked = []
 
     for first in vocab:
         for second in vocab:
             curr_pair = first + second
-            if curr_pair not in vocab_set and curr_pair in all_tokens:
+            if curr_pair in vocab_set:
+                pass
+            elif curr_pair in all_tokens.keys():
                 pairs[curr_pair] = all_tokens[curr_pair]
                 if pairs[curr_pair] > highest_freq:
                     highest_freq = pairs[curr_pair]
                     most_freq_word = curr_pair
-            elif curr_pair not in vocab_set and curr_pair not in pairs:
-                curr_freq = sum(token.count(curr_pair) for token in tokens)
-                pairs[curr_pair] = curr_freq
-                if curr_freq > highest_freq:
-                    highest_freq = curr_freq
+                    first_word = first
+                    second_word = second
+            elif curr_pair not in pairs_checked:
+                pairs_checked.append(curr_pair)
+                pairs[curr_pair] = 0
+                for token in tokens_dict.keys():
+                    curr_freq = token.count(curr_pair) * tokens_dict[token]
+                    pairs[curr_pair] += curr_freq
+                all_tokens[curr_pair] = pairs[curr_pair]
+                if pairs[curr_pair] > highest_freq:
+                    highest_freq = pairs[curr_pair]
                     most_freq_word = curr_pair
+                    first_word = first
+                    second_word = second
 
-    return pairs, most_freq_word
+    return pairs, most_freq_word, first_word, second_word
 
 def BPE(tokens, vocab_size):
-    vocab = Counter()
-    all_tokens = Counter()
+    vocab = {}
+    all_tokens = {}
     merge_rules = []
+    tokens_dict = {}
 
     for token in tokens:
-        vocab.update(token)
-        all_tokens.update(token)
-
-    for _ in range(vocab_size - len(vocab)):
-        pairs, most_freq_word = calculate_pair_freqs(tokens, vocab, all_tokens)
-
-        all_tokens.update(pairs)
-
-        first, second = most_freq_word[:1], most_freq_word[1:]
+        for char in token:
+            if char in vocab.keys():
+                vocab[char] += 1
+            else:
+                vocab[char] = 1
+            if char in all_tokens.keys():
+                vocab[char] += 1
+            else:
+                vocab[char] = 1
+        if token in tokens_dict.keys():
+            tokens_dict[token] += 1
+        else:
+            tokens_dict[token] = 1
+    
+    for _ in range(vocab_size - len(vocab.keys())):
+        pairs, most_freq_word, first, second = calculate_pair_freqs(tokens_dict, vocab.keys(), all_tokens)
+        
         merge_rules.append([first, second])
         vocab[most_freq_word] = pairs[most_freq_word]
-        vocab.subtract({first: pairs[most_freq_word], second: pairs[most_freq_word]})
-
-        print(most_freq_word + " is the new word, and the current size is " + str(len(vocab)) + "\n")
-
-    return vocab, merge_rules
-
-def BPE2(tokens, vocab_size):
-    vocab = Counter()
-    all_tokens = Counter()
-    merge_rules = []
-
-    for token in tokens:
-        vocab.update(token)
-        all_tokens.update(token)
-
-    priority_queue = PriorityQueue()
-
-    for _ in range(vocab_size - len(vocab)):
-        # Use a priority queue for efficient retrieval of the most frequent pair
-        priority_queue.queue.clear()
-        pairs_checked = set()
-        for first in vocab:
-            for second in vocab:
-                curr_pair = first + second
-                if curr_pair not in vocab and curr_pair not in all_tokens and curr_pair not in pairs_checked:
-                    pairs_checked.add(curr_pair)
-                    curr_freq = sum(token.count(curr_pair) for token in tokens)
-                    all_tokens.update({curr_pair: curr_freq})
-                    priority_queue.put((-curr_freq, curr_pair))
-                elif curr_pair not in vocab and curr_pair in all_tokens:
-                    pairs_checked.add(curr_pair)
-                    curr_freq = all_tokens[curr_pair]
-                    priority_queue.put((-curr_freq, curr_pair))
-        pairs_checked.clear()
-
-        most_freq_word = priority_queue.get()[1]
-
-        first, second = most_freq_word[:1], most_freq_word[1:]
-        merge_rules.append([first, second])
-        vocab[most_freq_word] = all_tokens[most_freq_word]
-        vocab.subtract({first: all_tokens[most_freq_word], second: all_tokens[most_freq_word]})
-
-        print(most_freq_word + " is the new word, and the current size is " + str(len(vocab)) + "\n")
-
-        # Call calculate_pair_freqs to update all_tokens and pairs
-        calculate_pair_freqs(tokens, vocab, all_tokens)
+        vocab[first] -= pairs[most_freq_word]
+        vocab[second] -= pairs[most_freq_word]
+        print(most_freq_word + " is new word and curr size is " + str(len(vocab.keys()))+ "\n")
 
     return vocab, merge_rules
 
@@ -308,7 +286,7 @@ if __name__ == "__main__":
 
         all_tokens.extend(tokens)
 
-    final_vocab, merge_rules = BPE2(all_tokens, vocab_size)
+    final_vocab, merge_rules = BPE(all_tokens, vocab_size)
 
     total_merge_rules = len(merge_rules)
     first_20_merge_rules = merge_rules[:20]
